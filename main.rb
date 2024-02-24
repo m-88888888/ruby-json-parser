06c2e15fb1b08b7e1a21d019203a5c58482ac8f3
m-88888888 <mochi.h8888@gmail.com>
2024-02-24 17:58:05 +0900

    refactor: tokenize

--- main.rb
+++ main.rb
@@ -3,46 +3,59 @@ class JsonLexer
 
   def initialize(input)
     @input = input
-    @tokens = []
-
-    tokenize
   end
-
-  private
 
   def tokenize
     i = 0
+    tokens = []
+
     while i < @input.length
       case @input[i]
       when '{', '}', '[', ']', ',', ':'
-        @tokens << { type: @input[i], value: @input[i] }
+        tokens << { type: @input[i], value: @input[i] }
         i += 1
       when '"'
         start = i
         i += 1
         i += 1 while @input[i] != '"'
-        @tokens << { type: 'STRING', value: @input[start + 1...i] }
+        tokens << { type: 'STRING', value: @input[start + 1...i] }
         i += 1
       when '0'..'9'
         start = i
         i += 1 while @input[i] =~ /[0-9]/
-        @tokens << { type: 'NUMBER', value: @input[start...i].to_i }
+        tokens << { type: 'NUMBER', value: @input[start...i].to_i }
       when /\s/
         i += 1
       else
         if @input[i..i + 3] == 'true'
-          @tokens << { type: 'TRUE', value: 'true' }
+          tokens << { type: 'TRUE', value: 'true' }
           i += 4
         elsif @input[i..i + 4] == 'false'
-          @tokens << { type: 'FALSE', value: 'false' }
+          tokens << { type: 'FALSE', value: 'false' }
           i += 5
         elsif @input[i..i + 3] == 'null'
-          @tokens << { type: 'NULL', value: 'null' }
+          tokens << { type: 'NULL', value: 'null' }
           i += 4
         else
           raise "Unknown token: #{@input[i]}"
         end
       end
+
+      tokens
     end
   end
+end
+
+json = File.read('./json/nest.json')
+tokens = JsonLexer.new(json).tokenize
+pp tokens
+
+class JsonParser
+  def initialize(tokens)
+    @tokens = tokens
+
+    @result = {}
+  end
+
+  def parse; end
 end
